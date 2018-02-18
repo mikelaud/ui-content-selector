171fffacea7d72070eba0c04adf517213cfd4fab
mikelaud <mmx.mmx@gmail.com>
2018-02-19 00:49:49 +0300

    add Accordion

--- src/main/java/com/blogspot/mikelaud/ui/UiContentSelector.java
+++ src/main/java/com/blogspot/mikelaud/ui/UiContentSelector.java
@@ -1,5 +1,29 @@
 package com.blogspot.mikelaud.ui;
 
-public class UiContentSelector {
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.StackPane;
+
+public class UiContentSelector extends StackPane {
+
+	private TitledPane newTitledPane(String aTitle) {
+		final TitledPane pane = new TitledPane();
+		pane.setText(aTitle);
+		pane.setContent(new Button(aTitle));
+		return pane;
+	}
+
+	private Accordion newAccordion() {
+		final Accordion accordion = new Accordion();
+		accordion.getPanes().add(newTitledPane("Test 1"));
+		accordion.getPanes().add(newTitledPane("Test 2"));
+		accordion.getPanes().add(newTitledPane("Test 3"));
+		return accordion;
+	}
+
+	public UiContentSelector() {
+		getChildren().add(newAccordion());
+	}
 
 }
--- src/test/java/com/blogspot/mikelaud/ui/UiContentSelectorDemo.java
+++ src/test/java/com/blogspot/mikelaud/ui/UiContentSelectorDemo.java
@@ -2,14 +2,13 @@ package com.blogspot.mikelaud.ui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class UiContentSelectorDemo extends Application {
 
 	@Override
 	public void start(Stage aPrimaryStage) throws Exception {
-		Scene scene = new Scene(new BorderPane(), 800, 600);
+		Scene scene = new Scene(new UiContentSelector(), 600, 800);
 		aPrimaryStage.setTitle(UiContentSelectorDemo.class.getSimpleName());
 		aPrimaryStage.setScene(scene);
 		aPrimaryStage.sizeToScene();
