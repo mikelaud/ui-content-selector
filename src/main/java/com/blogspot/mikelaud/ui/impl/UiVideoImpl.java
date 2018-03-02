03a4844363b790f5b9b127aa7805229cea14d85a
mikelaud <mmx.mmx@gmail.com>
2018-03-02 06:58:07 +0300

    add layoutChildren

--- src/main/java/com/blogspot/mikelaud/ui/impl/UiVideoImpl.java
+++ src/main/java/com/blogspot/mikelaud/ui/impl/UiVideoImpl.java
@@ -5,6 +5,11 @@ import com.google.inject.Inject;
 
 public class UiVideoImpl extends UiVideo {
 
+	@Override
+	protected void layoutChildren() {
+		// void
+	}
+
 	@Inject
 	private UiVideoImpl() {
 		// void
