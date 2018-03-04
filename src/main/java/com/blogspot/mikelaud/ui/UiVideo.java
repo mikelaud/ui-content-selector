9912f7b98421a02d956369551f9952abb03bf4e8
mikelaud <mmx.mmx@gmail.com>
2018-03-04 23:30:35 +0300

    add properties

--- src/main/java/com/blogspot/mikelaud/ui/UiVideo.java
+++ src/main/java/com/blogspot/mikelaud/ui/UiVideo.java
@@ -1,10 +1,14 @@
 package com.blogspot.mikelaud.ui;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.scene.layout.Region;
 import javafx.scene.media.MediaView;
 
 public abstract class UiVideo extends Region {
 
-	public abstract MediaView getMediaView();
+	public abstract ObjectProperty<MediaView> mediaViewProperty();
+	public abstract DoubleProperty volumeZoomProperty();
+	public abstract DoubleProperty volumeProperty();
 
 }
--- src/main/java/com/blogspot/mikelaud/ui/impl/UiVideoImpl.java
+++ src/main/java/com/blogspot/mikelaud/ui/impl/UiVideoImpl.java
@@ -3,9 +3,18 @@ package com.blogspot.mikelaud.ui.impl;
 import com.blogspot.mikelaud.ui.UiVideo;
 import com.google.inject.Inject;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
 import javafx.scene.media.MediaView;
 
 public class UiVideoImpl extends UiVideo {
+
+	private final ObjectProperty<MediaView> MEDIA_VIEW;
+	private final DoubleProperty VOLUME_ZOOM;
+	private final DoubleProperty VOLUME;
 
 	@Override
 	protected void layoutChildren() {
@@ -14,12 +23,13 @@ public class UiVideoImpl extends UiVideo {
 
 	@Inject
 	private UiVideoImpl() {
-		// void
+		MEDIA_VIEW = new SimpleObjectProperty<>(null);
+		VOLUME_ZOOM = new SimpleDoubleProperty(0.0005);
+		VOLUME = new SimpleDoubleProperty(0);
 	}
 
-	@Override
-	public MediaView getMediaView() {
-		return null;
-	}
+	@Override public ObjectProperty<MediaView> mediaViewProperty() { return MEDIA_VIEW; }
+	@Override public DoubleProperty volumeZoomProperty() { return VOLUME_ZOOM; }
+	@Override public DoubleProperty volumeProperty() { return VOLUME; }
 
 }
