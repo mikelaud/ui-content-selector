f6f854f8f0e2302f7d99d0c72bd8b1ad5bff4b8f
mikelaud <mmx.mmx@gmail.com>
2018-03-08 00:02:43 +0300

    open video

--- src/main/java/com/blogspot/mikelaud/ui/impl/UiContentViewerImpl.java
+++ src/main/java/com/blogspot/mikelaud/ui/impl/UiContentViewerImpl.java
@@ -1,8 +1,5 @@
 package com.blogspot.mikelaud.ui.impl;
 
-import java.io.File;
-import java.net.URI;
-
 import com.blogspot.mikelaud.ui.UiContentSelector;
 import com.blogspot.mikelaud.ui.UiContentViewer;
 import com.blogspot.mikelaud.ui.UiVideo;
@@ -14,8 +11,8 @@ public class UiContentViewerImpl extends UiContentViewer {
 	private UiVideo UI_VIDEO;
 
 	private void buildUi() {
-		final URI mediaUri = new File("video.mp4").toURI();
-		UI_VIDEO.setOnMouseClicked(e -> UI_VIDEO.openMedia(mediaUri.toString()));
+		final String mediaUri = getClass().getClassLoader().getResource("video.mp4").toExternalForm();
+		UI_VIDEO.setOnMouseClicked(e -> UI_VIDEO.openMedia(mediaUri));
 		getItems().addAll(UI_VIDEO, UI_CONTENT_SELECTOR);
 		setDividerPositions(0.6);
 		setMinSize(0, 0);
