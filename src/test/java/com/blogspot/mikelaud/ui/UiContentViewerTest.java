7b784acee1f2b1f7b7ad9f221209b61f6562c702
mikelaud <mmx.mmx@gmail.com>
2018-03-14 22:41:01 +0300

    add test stubs

---
+++ src/test/java/com/blogspot/mikelaud/ui/UiContentViewerTest.java
@@ -0,0 +1,20 @@
+package com.blogspot.mikelaud.ui;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UiContentViewerTest {
+
+	@Before
+	public void before() {
+		// void
+	}
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
